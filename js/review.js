2fd603e3d9e7e08aed5035e9499e13e07f30ebf4
Jessie Zhang <jessielz@uw.edu>
2015-10-30 08:32:16 -0700

    updated the review system

--- js/review.js
+++ js/review.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
 
 	// creates stars
-	var currentStar = $('#star').raty({ 
-		half: true,
-		targetKeep: true,
-		targetType: 'score',
-		targetFormat: 'ol'
-	});
+	var rating = $('#rating').raty({ 
+		half: true
+	});	
 
-	// counts the total number of stars (initially 0) 
-	var total = 0;
-
-	// counts the number of reviews submitted (initially 0)
-	var reviews = 0;
+	// creates average star rating
+	var avg = $('#average').raty({
+		readOnly: true,
+		noRatedMsg: "Average rating"
+	})
 
 	// Initialize Parse app.
 	Parse.initialize("UHSMBYE4dIvawri1wYJr0TcOI2CCnz4113TgEGBU","eQZlIILQ5m9qg4xbrpqKPfdAGbYhHjFSUwQzddXZ")
 
 	var Review = Parse.Object.extend('Review')
+
+	var totalReviews; // total number of reviews
+	var totalStars; // total number of stars
 
 	$('form').submit(function() {
 		// creates a new instance of review
@@ -25,10 +25,9 @@ $(document).ready(function(){
 
 		// sets the property for the title of the review
 		// and then clears the title afterwards
-		$(this).find('input').each(function() {
-			review.set($(this).attr('id'), $(this).val())
-			$(this).val(' ')
-		})
+		var title = $('#title').val();
+		review.set('title', title);
+		$('#title').val(' ')
 
 		// sets the property for the text area (body of review)
 		// and then clears the body afterwards
@@ -38,9 +37,9 @@ $(document).ready(function(){
 
 		// sets the property for the star rating system
 		// and then clears the star rating system afterwards
-		var rating = $('#star').raty('score');
-		review.set('rating', parseInt(rating))
-		$('#star').raty('score', 0)
+		var rating = $('#rating').raty('score');
+		review.set('rating', parseInt(rating));
+		$('#rating').raty({score: 0})
 
 		// saves the review (title, body, and rating) to Parse database
 		review.save(null, {
@@ -50,10 +49,10 @@ $(document).ready(function(){
 		return false
 	})
 
-	// gets data
+	// gets data from Parse
 	var getData = function() {
 		var query = new Parse.Query(Review)
-		query.notEqualTo('star', '')
+		query.notEqualTo('title', '')
 		query.find({
 			success:function(results) {
 				buildList(results)
@@ -61,26 +60,47 @@ $(document).ready(function(){
 		})
 	}
 
+	// builds the list of reviews
 	var buildList = function(data) {
 		$('#list').empty()
+		totalStars = 0;
+		totalReviews = 0;
 		data.forEach(function(d){
-			total += d.get("rating")
+			totalStars += d.get("rating");
+			totalReviews++
 			addItem(d)
 		})
-		reviews = data.length
 	}
 
+	// adds the list of reviews to the page
 	var addItem = function(item) {
 		var title = item.get("title") // title of the review
 		var body = item.get("body") // body of the review (review itself)
-		var rating = item.get("star") // rating of the item
-		total += rating
-
-		$('star').raty({
-			readOnly: true
-			score: total / reviews
+		var rating = item.get("rating") // rating of the item
+		avg = $('#average').raty({
+			readOnly: true,
+			score: Number(totalStars) / Number(totalReviews)
 		})
-	}
+		// construct a review
+		var addReview = $('<div><div class = "stars"></div><h3>' + title + '</h3><strong>Rating: ' + rating + '</strong> | <span>' + body + '</span></div>')
+		$('.stars').raty({
+			readOnly: true,
+			score: rating
+		})
 
+		// creates a button with a <span>element (using bootstrap class to show the X)
+		var button = $('<button class = "btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button>')
+		
+		// click function on the button to destroy the item and then recall getData
+		button.click(function(){
+			item.destroy({
+				success: getData
+			})
+		})
+
+		addReview.append(button)
+		$('#list').append(addReview)
+		// $('#list').append(addReview)
+	}
 	getData()
 })
